b47dfc73da944b384472b9266c9f64caf31930e7
AustinHadden <vitovalletori@gmail.com>
2020-01-24 11:55:37 -0600

    made and styled a toggle switch component

---
+++ src/components/SearchBar&Filters.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Switch from "./Switch";
+
+export const SeachBar = () => {
+
+};
+
+export const Filters = () => {
+
+    return (
+        <div className="filter-list">
+
+        </div>
+    )
+}
