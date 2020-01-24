fe805b52b30cc6fe942dd653c9f00ddd674e3f2b
AustinHadden <vitovalletori@gmail.com>
2020-01-24 12:41:43 -0600

    made a filters list

--- src/components/SearchBar&Filters.js
+++ src/components/SearchBar&Filters.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ToggleSwitch from "./ToggleSwitch";
 
 export const SeachBar = () => {
@@ -6,10 +6,21 @@ export const SeachBar = () => {
 };
 
 export const Filters = () => {
+    const [intern, setIntern] = useState(false);
+    const [junior, setJunior] = useState(false);
+    const [midLevel, setMidLevel] = useState(false);
+    const [senior, setSenior] = useState(false);
+    const [contract, setContract] = useState(false);
+    const [remote, setRemote] = useState(false);
 
     return (
         <div className="filter-list">
-
+            <label>Internship <ToggleSwitch isOn={intern} handleToggle={() => setIntern(!intern)} /></label>
+            <label>Junior Position <ToggleSwitch isOn={junior} handleToggle={() => setJunior(!junior)} /></label>
+            <label>Mid-level Position <ToggleSwitch isOn={midLevel} handleToggle={() => setMidLevel(!midLevel)} /></label>
+            <label>Senior Position <ToggleSwitch isOn={senior} handleToggle={() => setSenior(!senior)} /></label>
+            <label>Contract <ToggleSwitch isOn={contract} handleToggle={() => setContract(!contract)} /></label>
+            <label>Remote <ToggleSwitch isOn={remote} handleToggle={() => setRemote(!remote)} /></label>
         </div>
     )
-}
+};
