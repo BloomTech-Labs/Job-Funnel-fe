43e44332ee9d31c568648602935a2d6420b7256c
AustinHadden <vitovalletori@gmail.com>
2020-01-27 13:01:38 -0600

    made blank dashboard object as placeholder

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,16 +1,11 @@
-import React, {useState} from "react"
+import React from "react"
 
 const Dashboard = () => {
-    const [open, setOpen] = useState(false)
-
-    const Toggled = () => {
-        setOpen(open => !open)
-    }
+    
     
     return (
         <div>
-            <button onClick={Toggled}>Open</button>
-            <div>{open && <h3>Testing Modal</h3>}</div>
+            
            
         </div>
     )
