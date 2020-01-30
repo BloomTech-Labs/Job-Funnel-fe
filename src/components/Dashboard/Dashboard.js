2b0e0ad6149d15f531242ec8005dcc7bc13ac92d
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2020-01-29 16:56:09 -0800

    changed topnav to navagation

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import DashboardCards from "./DashboardCards";
-import {SearchBar, Filters} from "./SearchBar&Filters";
+import { SearchBar, Filters } from "./SearchBar&Filters";
 import CardTopNav from "./CardTopNav";
-import TopNav from "./TopNav";
+import Navigation from "./Navigation";
 import SuggestedJobs from "./SuggestedJobs.js";
 import '../../less/ComponentStyles/Dashboard.less'
 
 export default function Dashboard() {
     return (
         <>
-            <TopNav/>
+            <Navigation />
             <div className="container">
                 <div className="left-nav">
-                    <SearchBar/>
-                    <Filters/>
+                    <SearchBar />
+                    <Filters />
                 </div>
                 <div className="card-items">
-                    <CardTopNav/>
-                    <DashboardCards/>
+                    <CardTopNav />
+                    <DashboardCards />
                     <SuggestedJobs />
                 </div>
             </div>
---
+++ src/components/Dashboard/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function TopNav() {
+    return (
+
+        <div className="top-nav">
+
+            <h1>QuickHire</h1>
+            <img src="https://www.aalforum.eu/wp-content/uploads/2016/04/profile-placeholder.png" className="profile-image" onclick="" />
+        </div>
+
+    )
+}
+
+export default TopNav;
