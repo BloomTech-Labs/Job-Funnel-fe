55f6ab08c698a224c761e3133e90251b8b4870aa
AustinHadden <vitovalletori@gmail.com>
2020-02-10 16:36:11 -0600

    made the job details page

--- src/components/Dashboard/Jobs/JobDetails.js
+++ src/components/Dashboard/Jobs/JobDetails.js
@@ -4,22 +4,37 @@ import axiosWithAuth from "../../../utils/axiosWithAuth.js"
 
 
 export default function JobDetails(props) {
-    console.log('job details', props);
 
-   // const [details, setDetails] = useState({});
+   const [details, setDetails] = useState({});
     
 
-    // useEffect(()=> {
-    //     axiosWithAuth().get(`/jobs/${props.match.params.id}`)
-    //         .then(response => {
-    //             console.log('job details axios response', response);
-    //         })
-    //         .catch(err => console.error(err))
-    // }, []);
+    useEffect(()=> {
+        axiosWithAuth().get(`/jobs/${props.match.params.id}`)
+            .then(response => {
+                console.log('job details axios response', response.data);
+                setDetails(response.data);
+            })
+            .catch(err => console.error(err))
+    }, []);
+
+    const postedDate = Date(details.post_date_utc)
+
     return (
-        <div>
-            <h1>JobDetails</h1>
+        <>
+        <div className="deets-apply-button">
+            <button>Apply to Job</button>
         </div>
+        <div className="deets-div">
+            <h2>{details.title}</h2>
+            <p className="company-name">{details.companyName}</p>
+            <p className="job-location">{details.city} {details.stateOrProvince}</p>
+            <p className="job-posting-date">{postedDate}</p>
+        </div>
+        
+        <div className="desc-div" >
+        <h2 className="job-description" >{details.description}</h2>
+        </div>
+        </>
     )
 }
 
