49ffcfa1ebeeafabac11beb92691cdea0188a4e6
Mauricio Degregori <degregori2@gmail.com>
2020-02-12 17:18:24 -0600

    delete job working, but not without hitting twice. fix bug

--- src/components/Dashboard/Jobs/JobCard.js
+++ src/components/Dashboard/Jobs/JobCard.js
@@ -17,23 +17,56 @@ function JobCard(props) {
         status: "saved"
     })
 
-    
-    
+    const [toggle, setToggle] = useState(false)
+
     const handleSave = () => {
         console.log('saved ', saved)
         axiosWithAuth().post('/saved/', saved)
-        .then(response => {
-            console.log('handle save job response', response.data)
+        .then(res => {
+            console.log('handle save job response', res.data)
             setSaved({...saved})
-            console.log('saved', saved)
-            
+            setToggle(true)
         })
         .catch(error => {
             console.error(error)
         })
-
     }
 
+    const handleDelete = () => {
+        axiosWithAuth().delete(`/saved/${job_id}`)
+        .then(res => {
+            console.log('erased job from saved table?', res.data)
+            setToggle(false)
+            setSaved({...saved})
+        })
+        .catch(error => {
+            console.error(error)
+        })
+    }
+
+    if(toggle === true){
+        return (
+            <div className="jobCard">
+                <div style={{display: 'flex', justifyContent: 'space-between', margin: '10px 15px', }}> 
+                    <h4>Your Skills match 6/7</h4>
+                </div>
+                <div className="card-image">
+                    <img className="image" src="http://pngimg.com/uploads/microsoft/microsoft_PNG18.png"/>
+                </div>
+                <div className="card-text">
+                    <h3>{props.title}</h3>
+                    <p>{props.company}</p>
+                    <span>📍 {props.location}</span>
+                </div>
+                <div className='jobButtons' >
+                    <button onClick={handleDelete}>Unsave</button>
+                    <Link to={`/Dashboard/Job/${props.id}`}>
+                        <button>View</button>
+                    </Link>
+                </div>
+            </div>
+        )
+    }
     return (
         <div className="jobCard">
             <div style={{display: 'flex', justifyContent: 'space-between', margin: '10px 15px', }}> 
