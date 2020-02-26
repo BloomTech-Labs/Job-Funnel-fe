ab641acea06d54114b0174cac8b6fb6f15220fb4
AustinHadden <vitovalletori@gmail.com>
2020-02-26 14:04:04 -0600

    fixing https

--- src/utils/searchAPI.js
+++ src/utils/searchAPI.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default function searchAPI() {
     return axios.create({
-        baseURL: 'http://quickhire-api-dev.j535vysrhe.us-east-1.elasticbeanstalk.com',
+        baseURL: 'https://quickhire-api-dev.j535vysrhe.us-east-1.elasticbeanstalk.com',
         headers: {
             'Content-Type': 'application/json',
         },
