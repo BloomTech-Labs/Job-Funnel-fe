31921ec15da336b1a7114fdc3e7fe15146e817f6
Mauricio Degregori <degregori2@gmail.com>
2020-02-28 15:32:17 -0600

    added 3 unit tests

--- src/tests/Register.test.js
+++ src/tests/Register.test.js
@@ -5,3 +5,25 @@ import { Provider } from "react-redux";
 import configureMockStore from "redux-mock-store";
 import Adapter from 'enzyme-adapter-react-16';
 import Register from "../components/Account/Register"
+
+const mockStore = configureMockStore();
+const store = mockStore({});
+
+test('register component renders', ()=>{
+    render ( <Provider store={store}><Register/></Provider>)
+})
+
+test('shows heading', ()=> {
+    const { getByText } = render(<Provider store={store}><Register/></Provider>);
+
+    getByText(/Make the most of your professional life./i);
+} )
+
+test('Check for labels on register form', ()=> {
+    const { getByText } = render(<Provider store={store}><Register/></Provider>);
+
+    getByText(/first name/i);
+    getByText(/last name/i);
+    getByText(/enter email/i);
+    getByText(/create password/i);
+} )
