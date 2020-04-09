fb3dfbdaef9d7a99ec3e9b0f25ddbdad04d9df32
kika <anamonteiro430@gmail.com>
2020-04-09 16:03:50 +0100

    LANDING PAGE

--- src/components/Account/LandingPage.js
+++ src/components/Account/LandingPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from '../../images/logo.jpg'
+import landing from '../../images/landing.png'
 
 
 
@@ -9,20 +9,16 @@ const LandingPage = () => {
     return (
         <section className='animated zoomInUp fast landing-page'>
             <div className='content-div'>
-                <div>
-                    <h1 >Welcome to your Quickhire Community.</h1>
-                </div>
-                <div className='p-div' >
-                    <p className='p-1'>You've been learning how to program, day in and day out, for the past nine months - Now, you're looking for your next, high-paying job.</p>
-                    <p className='p-2'>But, finding that job is a challenge, and the process as a whole, is broken. </p>
-                    <p className='p-3'> Join us at Quickhire.dev, where we help you find tech jobs, tailored completely to you. </p>
+                <div className="landing-info">
+                    <h1 >Welcome to your <br />Quickhire Community.</h1>
+                    <p className='p-1'>You just finish your Bootcamp and want to find a job.
+                    Join us in job funnel and we will help you to find your match!</p>
+                    <div className='animated flip button-div'><Link to="/Register"> <button >Join Now!</button></Link></div>
                 </div>
 
-                
-                <div className='bottom-content'>
-                    <div className='animated flip button-div'><Link to="/Register"> <button >Join Now!</button></Link></div>
-                    <div className='img-div'><img src={logo} /></div>
-                </div>
+                <div className='img-div'><img src={landing} width="600" /></div>
+
+
 
             </div>
 
